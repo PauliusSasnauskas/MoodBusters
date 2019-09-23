3131959bc3b0480301eac1d0aed9e7ea9804b9cd
Jurgelevičius Gintautas <gintautas.jurgelevicius@mif.stud.vu.lt>
2019-09-23 12:16:18 +0300

    Fix bad merge

--- Mood Busters/Form1.Designer.cs
+++ Mood Busters/Form1.Designer.cs
@@ -73,7 +73,6 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Mood Buster";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
