7716d9a6a8d1c4a54c39af84ad8e97ca7e690bf9
Ingvaras Galinskas <ingvaraz4@gmail.com>
2019-09-26 03:39:02 +0300

    Database reading and writing
    Added DatabaseConnection Class with SaveData and LoadData methods. Also created a freely hosted database for this project. Did spent quite some time with load_data.php and save_data.php files. These files are currently uploaded in the web, I just simply dropped them in the project folder for better understanding of what they do. To learn what my mood was like when I was coding, load data from the database with id = 1;

--- Mood Busters/Program.cs
+++ Mood Busters/Program.cs
@@ -14,7 +14,8 @@ namespace Mood_Busters
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
+            //DatabaseConnection.SaveData("CALM", "2019-09-26 03:28:09", "Lithuania");            TESTING DATABASE
+            //Console.WriteLine(DatabaseConnection.LoadData("1"));
         }
     }
 }
