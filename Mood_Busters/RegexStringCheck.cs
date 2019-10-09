3371b3c3aa62dd29c7f16b9a47b8bb27b13b0774
Jurgelevičius Gintautas <gintautas.jurgelevicius@mif.stud.vu.lt>
2019-10-10 00:30:00 +0300

    Update new object creation everytime

--- Mood_Busters/RegexStringCheck.cs
+++ Mood_Busters/RegexStringCheck.cs
@@ -5,9 +5,11 @@ namespace Mood_Busters
     static class RegexStringCheck
     {
         private const string pattern = "((Error|Unknown|Happy|Sad|Angry|Confused|Disgusted|Surprised|Calm|Fear) (?:100|[1-9]?[0-9])%)";
+        private static Regex rx = new Regex(pattern);
+
         public static bool checkString(string text)
         {  
-            return (new Regex(pattern)).IsMatch(text);
+            return (rx.IsMatch(text));
         }
     }
 }
