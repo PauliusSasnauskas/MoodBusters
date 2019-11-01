da78201f1b5200d2974f3bab193f9c5d065f6df5
Jurgelevičius Gintautas <gintautas.jurgelevicius@mif.stud.vu.lt>
2019-11-02 00:49:23 +0200

    Add dispose, rename some variables, refactor code a bit.

---
+++ AndroMooda3/Callbacks/CameraCaptureSessionCallback.cs
@@ -0,0 +1,29 @@
+﻿
+using Android.Hardware.Camera2;
+using Android.Support.Design.Widget;
+
+namespace AndroMooda3.Callbacks
+{
+    public class CameraCaptureSessionCallback : CameraCaptureSession.StateCallback
+    {
+        private MainActivity activity;
+        private CameraImplementation ci;
+
+        public CameraCaptureSessionCallback(MainActivity activity, CameraImplementation ci)
+        {
+            this.activity = activity;
+            this.ci = ci;
+        }
+
+        public override void OnConfigured(CameraCaptureSession session)
+        {
+            ci.captureRequestBuilder.Set(CaptureRequest.ControlMode, 1);
+            session.SetRepeatingRequest(ci.captureRequestBuilder.Build(), null, null);
+        }
+
+        public override void OnConfigureFailed(CameraCaptureSession session)
+        {
+            Snackbar.Make(activity.rootView, "OnConfigureFailed", Snackbar.LengthShort).Show();
+        }
+    }
+}
---
+++ AndroMooda3/Callbacks/CameraDeviceCallback.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Hardware.Camera2;
+using Android.Hardware.Camera2.Params;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace AndroMooda3.Callbacks
+{
+    public class CameraDeviceCallback : CameraDevice.StateCallback
+    {
+        private Surface surface;
+        private MainActivity activity;
+        private CameraImplementation ci;
+
+        public CameraDeviceCallback(MainActivity activity, CameraImplementation ci, Surface surface)
+        {
+            this.ci = ci;
+            this.surface = surface;
+            this.activity = activity;
+        }
+
+        public override void OnDisconnected(CameraDevice camera)
+        {
+            camera.Close();
+        }
+
+        public override void OnError(CameraDevice camera, [GeneratedEnum] CameraError error)
+        {
+            Snackbar.Make(activity.rootView, "CameraDevice State Error", Snackbar.LengthShort).Show();
+        }
+
+        public override void OnOpened(CameraDevice camera)
+        {
+            List<OutputConfiguration> loc = new List<OutputConfiguration>();
+            loc.Add(new OutputConfiguration(surface));
+            SessionConfiguration cfg = new SessionConfiguration(0, loc, AsyncTask.ThreadPoolExecutor, new CameraCaptureSessionCallback(activity, ci));
+            camera.CreateCaptureSession(cfg);
+            ci.captureRequestBuilder = camera.CreateCaptureRequest(CameraTemplate.Preview);
+            ci.captureRequestBuilder.AddTarget(surface);
+        }
+    }
+    }
