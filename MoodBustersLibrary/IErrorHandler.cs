6b5a8083ce5ef4e17c2e81143269a85c1a1795ab
Justas <47787752+Justas@users.noreply.github.com>
2019-11-11 17:28:44 +0200

    Generics
    Implemented generics in methods.

--- MoodBustersLibrary/IErrorHandler.cs
+++ MoodBustersLibrary/IErrorHandler.cs
@@ -2,6 +2,7 @@ namespace MoodBustersLibrary
 {
     public interface IErrorHandler
     {
+        void GenericMethod<T>(ref T eT, ref T eR);
         void ShowError(string errorText, string errorName = "Error");
         void HandleAndExit(string errorText, string errorName = "Error");
     }
--- Mood_Busters/ErrorHandlerWindows.cs
+++ Mood_Busters/ErrorHandlerWindows.cs
@@ -6,9 +6,15 @@ namespace Mood_Busters
 {
     public class ErrorHandlerWindows : IErrorHandler
     {
+        void GenericMethod<T>(ref T eT, ref T eR)
+        {
+            MessageBox.Show(eT.ToString(), eR.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
         public void ShowError(string errorText, string errorName = "Error")
         {
-            MessageBox.Show(errorText, errorName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //MessageBox.Show(errorText, errorName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            GenericMethod(ref errorText, ref errorName); //Now uses Generic Method to print out the same thing
         }
 
         public void HandleAndExit(string errorText, string errorName = "Error")
