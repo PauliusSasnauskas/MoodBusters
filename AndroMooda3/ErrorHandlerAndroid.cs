342395f9f2ab6f6951112cafa2e6a654a3f9ccf3
Jurgelevičius Gintautas <gintautas.jurgelevicius@mif.stud.vu.lt>
2019-11-28 00:01:52 +0200

    Fix error handler HandleAndExit method with delay

---
+++ AndroMooda3/ErrorHandlerAndroid.cs
@@ -0,0 +1,40 @@
+﻿using MoodBustersLibrary;
+using Android.App;
+using Plugin.CurrentActivity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AndroMooda3
+{
+    public class ErrorHandlerAndroid : IErrorHandler
+    {
+        private Activity activity = CrossCurrentActivity.Current.Activity;
+
+        public void ShowError(string errorText, string errorName = "Error")
+        {
+            AlertDialog.Builder alert = new AlertDialog.Builder(activity);
+            alert.SetTitle(errorName);
+            alert.SetMessage(errorText);
+            alert.Show();
+        }
+
+        public async void HandleAndExit(string errorText, string errorName = "Error")
+        {
+            ShowError(errorText, errorName);
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            activity.FinishAffinity();
+        }
+
+        public void HandleException(Exception e, string errorName = "Error")
+        {
+            ShowError(e.Message, errorName);
+
+            if(e.InnerException != null)
+            {
+                HandleException(e.InnerException, errorName);
+            }
+        }
+
+    }
+}
