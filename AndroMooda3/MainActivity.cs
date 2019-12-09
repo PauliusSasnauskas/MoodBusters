4efc5ba69e4edad184fb7d1d62ccb0e2337404c7
Jurgelevičius Gintautas <gintautas.jurgelevicius@mif.stud.vu.lt>
2019-12-09 22:32:51 +0200

    Add gallery selection

--- AndroMooda3/MainActivity.cs
+++ AndroMooda3/MainActivity.cs
@@ -10,6 +10,8 @@ using Android.Views;
 using MoodBustersLibrary;
 using Plugin.CurrentActivity;
 using System.Text;
+using Android.Content;
+using Android.Widget;
 
 namespace AndroMooda3
 {
@@ -20,6 +22,9 @@ namespace AndroMooda3
         RecyclerView.LayoutManager layoutManager;
         RecyclerView.Adapter imageAdapter;
         IErrorHandler errorHandler;
+        ImageList imageList;
+
+        public static readonly int PickImageId = 1000;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -36,31 +41,45 @@ namespace AndroMooda3
             layoutManager = new GridLayoutManager(this, 4);
             recyclerView.SetLayoutManager(layoutManager);
 
-            List<ImageItem> items = new List<ImageItem>();
-            for (int i = 0; i < 6; i++)
-            {
-                items.AddRange(new List<ImageItem>{
+            List<ImageItem> images = new List<ImageItem>{
                     new ImageItem(BitmapFactory.DecodeResource(Resources, Resource.Drawable.face1), MoodName.Happy),
                     new ImageItem(BitmapFactory.DecodeResource(Resources, Resource.Drawable.face2), MoodName.Calm),
                     new ImageItem(BitmapFactory.DecodeResource(Resources, Resource.Drawable.face3), MoodName.Angry),
                     new ImageItem(BitmapFactory.DecodeResource(Resources, Resource.Drawable.face4), MoodName.Sad),
-                    new ImageItem(BitmapFactory.DecodeResource(Resources, Resource.Drawable.face5), MoodName.Disgusted)
-                });
-            }
+                    new ImageItem(BitmapFactory.DecodeResource(Resources, Resource.Drawable.face5), MoodName.Disgusted),
+            };
 
-            items = items.Skip(4).Take(25).ToList();
+            imageList = new ImageList();
+            imageList.Images.AddRange(images);
 
-            string feelings = items.Aggregate("People are feeling ", (string acc, ImageItem item) => acc + item.mood.ToString() + ", ");
+            //imageList.Images = imageList.Images.Skip(4).Take(25).ToList();
+
+
+            string feelings = imageList.Images.Aggregate("People are feeling ", (string acc, ImageItem item) => acc + item.mood.ToString() + ", ");
 
             Snackbar.Make(recyclerView.RootView, feelings, Snackbar.LengthShort).Show();
 
-            imageAdapter = new ImageGridAdapter(this, items);
+            imageAdapter = new ImageGridAdapter(this, imageList.Images);
             recyclerView.SetAdapter(imageAdapter);
 
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.btn_add_image);
             fab.Click += (sender, eventArgs) => {
-                errorHandler.HandleAndExit("Something went wrong...");
-            };
+                Intent = new Intent();
+                Intent.SetType("image/*");
+                Intent.SetAction(Intent.ActionGetContent);
+                StartActivityForResult(Intent.CreateChooser(Intent, "Select Picture"), PickImageId);
+            };  
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            if ((requestCode == PickImageId) && (resultCode == Result.Ok) && (data != null))
+            {
+                Android.Net.Uri uri = data.Data;
+                imageList.AddImage(uri);
+                imageAdapter.NotifyDataSetChanged();
+            }
+            else Toast.MakeText(this, "No image was added.", ToastLength.Short).Show();
         }
     }
 
